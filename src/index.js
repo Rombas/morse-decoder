cc389e6878f51c35a746b0d4d9815d2eb35095e4
Rombas <RomanBash@gmail.com>
2020-10-21 15:58:09 +0300

    feat: realized morse decoder function

--- src/index.js
+++ src/index.js
@@ -39,7 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    let res = '';
+    let buf = '';
+    for (let i = 0; i < expr.length; i = i + 10){
+      buf = '';  
+      for (j = 0; j < 10; j += 2){
+            if (expr[i+j] === '*'){
+              buf += ' ';
+              break;
+            }
+            if (expr[i+j] === '1' && expr[i+j+1] === '0')
+            buf += '.';
+            else if (expr[i+j] === '1' && expr[i+j+1] === '1')
+            buf += '-';
+        }
+        if (buf === ' ') res += ' ';
+        else res += MORSE_TABLE[buf];
+    }
+    return res;
+  }
 
 module.exports = {
     decode
